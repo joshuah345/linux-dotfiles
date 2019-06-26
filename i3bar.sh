66bbe5ec5c74df7a0b0d0088fba46c0d9a43d5a6
Joshua Henry <joshuah345@gmail.com>
2019-06-26 01:03:55 -0400

    typo fix

--- i3bar.sh
+++ i3bar.sh
@@ -4,7 +4,7 @@ pgrep polybar
 
 if [$? -eq 0];
 
-then pkill polybar & polybar -c ~/.comfig/polybar/aenami.ini aenami
+then pkill polybar & polybar -c ~/.config/polybar/aenami.ini aenami
 
 else polybar -c ~/.config/polybar/aenami.ini aenami
 
