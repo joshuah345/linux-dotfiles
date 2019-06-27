545fd2ccb0064473737e72f9e131373050a57789
joshuah345 <joshuah345@gmail.com>
2019-06-26 22:01:46 -0400

    i3bar new

--- i3bar.sh
+++ i3bar.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
+pkill polybar && notify-send --app-name Polybar "All running instances of polybar have been killed"
+shopt -s extglob
+pdir=$HOME/.config/polybar
 
-pgrep polybar 
+case $1 in 
+    !(list))
+    fname=$1
+    ;;
+    list) 
+    ls --color $HOME/.config/polybar
+esac
 
-if [$? -eq 0];
+case $2 in
+    *)
+    bname=$2 
+esac 
 
-then pkill polybar & polybar -c ~/.comfig/polybar/aenami.ini aenami
+ [[ -z "$2" ]] && exit || polybar -r -c $pdir/$fname $bname 1>&- 2>&-  & 
+ notify-send --app-name Polybar "Bar "$bname" has started." 
 
-else polybar -c ~/.config/polybar/aenami.ini aenami
 
-fi
